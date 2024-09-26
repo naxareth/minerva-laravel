9d2b0cd462b1f657b1e2f4b14e9b7683db5e555c
naxareth <acephilipdenulan12@gmail.com>
2024-09-26 20:24:50 +0800

    added authentication

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,21 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\FavoritesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('favorites', FavoritesController::class);
+
+Route::post('/login',[AuthController::class,'login']);
+Route::post('/register',[AuthController::class,'register']);
+
+
+Route::middleware(['auth:sanctum'])->group(function(){
+
+    Route::post('/logout',[AuthController::class,'logout']);
+    Route::get('/profile',[AuthController::class,'profile']);
+    Route::apiResource('favorites', FavoritesController::class);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
